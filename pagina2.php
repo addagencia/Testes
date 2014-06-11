644b8ef2a7582f21a40dec6dab9a9070b540f38a
addagencia <thiago@add-digital.com.br>
2014-06-11 19:00:48 -0300

    Adição de página

--- pagina2.php
+++ pagina2.php
@@ -2,18 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Hello World</title>
+<title>Página 2</title>
 </head>
 
 <body>
 
 <?PHP
-	echo "
-		HelloWorld<br />
-		<a href='pagina2.php'>Clique aqui</a> para ir para a página 2
-		<br />
-	";
-	echo phpinfo();
+	echo "Bem vindo a segunda página";
 ?>
 
 </body>
